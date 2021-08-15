ce71d7cdec66639fc2198e8b4005966643b270af
Juan Buitrago <juanbuitrago2002@outlook.com>
2021-08-15 12:28:38 -0500

    shoes icon

--- src/components/SectionMiddle.js
+++ src/components/SectionMiddle.js
@@ -9,38 +9,42 @@ import {
   StyledDiv5,
   StyledDiv6,
   StyledDiv7,
-  StyledImg,
-  StyledImg100pre,
+  Img100pre,
   StyledAlbum,
+  Img,
 } from "../Styles/styledSectionMiddle";
 
 import conejo from "../images/icons/icons_svg/bad bunny logo.svg";
 import oasis from "../images/oasis.jpg";
 import x100pretext from "../images/icons/icons_svg/x100prelogo.svg";
-import tenis from "../images/icons/icons_svg/x100prelogo.svg";
+
+import BlackShoes from "../images/icons/badbunnyshoes.svg";
+import BrownShoes from "../images/icons/brownshoes.svg";
+import RoseShoes from "../images/icons/roseShoes.svg";
+
 export default function SectionMiddle() {
   return (
     <>
       <StyledContainerHome>
         <StyledSectionMiddle>
           <StyledDiv1>
-            <StyledImg src={conejo} alt="conejo" />
+            <Img src={conejo} alt="conejo" />
           </StyledDiv1>
           <StyledDiv2>
             <StyledAlbum src={oasis} alt="oasis" />
           </StyledDiv2>
           <StyledDiv3>❤</StyledDiv3>
           <StyledDiv4>
-            <StyledImg100pre src={x100pretext} alt="x100pretext" />
+            <Img100pre src={x100pretext} alt="x100pretext" />
           </StyledDiv4>
           <StyledDiv5>
-            <StyledImg src={tenis} alt="tenis" />
+            <Img src={BlackShoes} alt="BlackShoes" />
           </StyledDiv5>
           <StyledDiv6>
-            <StyledImg src={tenis} alt="tenis" />
+            <Img src={RoseShoes} alt="RoseShoes" />
           </StyledDiv6>
           <StyledDiv7>
-            <StyledImg src={tenis} alt="tenis" />
+            <Img src={BrownShoes} alt="BrownShoes" />
           </StyledDiv7>
         </StyledSectionMiddle>
       </StyledContainerHome>
