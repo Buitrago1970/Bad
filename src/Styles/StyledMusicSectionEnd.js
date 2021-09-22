fe51e7fadd2962839de20dac63f37b75824013d5
Juan Buitrago <juanbuitrago2002@outlook.com>
2021-09-22 15:06:41 -0500

    diesño mobile section music

--- src/Styles/StyledMusicSectionEnd.js
+++ src/Styles/StyledMusicSectionEnd.js
@@ -1,7 +1,11 @@
 import styled from "styled-components";
+import { device } from "../../breakpoints";
 
 export const StyledContainerHome = styled.div`
   margin: 0 37px;
+  @media ${device.sm} {
+    margin: 0 10px;
+  }
 `;
 // ------------------------
 export const Music = styled.div`
@@ -10,26 +14,49 @@ export const Music = styled.div`
   grid-template-rows: repeat(4, 1fr);
   grid-column-gap: 0px;
   grid-row-gap: 0px;
+  @media ${device.sm} {
+    grid-row-gap: 5px;
+  }
 `;
 export const StyledDiv1 = styled.div`
   grid-area: 1 / 3 / 2 / 4;
+  @media ${device.sm} {
+    width: 120%;
+    width: 120%;
+  }
 `;
 export const StyledDiv2 = styled.div`
   grid-area: 2 / 3 / 3 / 4;
+  @media ${device.sm} {
+    width: 120%;
+  }
 `;
 export const StyledDiv3 = styled.div`
   grid-area: 3 / 5 / 4 / 6;
+  @media ${device.sm} {
+    width: 120%;
+  }
 `;
 export const StyledDiv4 = styled.div`
   grid-area: 2 / 5 / 3 / 6;
   text-shadow: -2px 2px 0px #ff99cc, -4px 0px 0px #64a5b7;
   font-size: 30px;
+  @media ${device.sm} {
+    width: 120%;
+    font-size: 20px;
+  }
 `;
 export const StyledDiv5 = styled.div`
   grid-area: 4 / 2 / 5 / 3;
+  @media ${device.sm} {
+    width: 120%;
+  }
 `;
 export const StyledDiv6 = styled.div`
   grid-area: 4 / 1 / 5 / 2;
+  @media ${device.sm} {
+    width: 120%;
+  }
 `;
 export const StyledDiv7 = styled.div`
   grid-area: 2 / 1 / 3 / 2;
@@ -37,12 +64,21 @@ export const StyledDiv7 = styled.div`
     0 0 4px #0b554c, 0 0 40px #0b554c;
   -webkit-text-stroke-color: hsl(0deg 46% 96% / 72%);
   font-size: 50px;
+  @media ${device.sm} {
+    font-size: 20px;
+  }
 `;
 export const StyledDiv8 = styled.div`
   grid-area: 3 / 4 / 4 / 5;
+  @media ${device.sm} {
+    width: 120%;
+  }
 `;
 export const StyledDiv9 = styled.div`
   grid-area: 5 / 3 / 6 / 4;
+  @media ${device.sm} {
+    width: 120%;
+  }
 `;
 export const Img1 = styled.img`
   width: 100%;
@@ -52,6 +88,8 @@ export const Img1 = styled.img`
     rgb(255 217 19) 20px -20px, rgb(0 0 17) 30px -30px 0px -3px,
     rgb(255 156 85) 30px -30px, rgb(0 0 17) 40px -40px 0px -3px,
     rgb(255 85 85) 40px -40px;
+  @media ${device.sm} {
+  }
 `;
 export const Img2 = styled.img`
   width: 100%;
@@ -59,20 +97,28 @@ export const Img2 = styled.img`
   box-shadow: rgb(255 255 255) 0px -1px 7px 3px, rgb(4 4 4) 0px -2px 5px 4px,
     rgb(4 2 5) -3px -3px 0px, #030102 0px -2px 5px 7px,
     -4px 3px 6px 0px rgb(2 2 2);
-  border: 2px outset #020202; ;
+  border: 2px outset #020202;
+  @media ${device.sm} {
+  }
 `;
 export const Img3 = styled.img`
   width: 100%;
   border-radius: 7px;
+  @media ${device.sm} {
+  }
 `;
 export const Img4 = styled.img`
   width: 100%;
   border-radius: 4px;
   border: 2px outset #ffffff;
-  
   box-shadow: #fff 0px 0px 4px, #ff0 0 -2px 10px, #ff8000 0 -10px 20px,
     red 0 -18px 40px, 5px 5px 12px 5px rgb(0 0 0 / 0%);
+  @media ${device.sm} {
+  }
 `;
 export const Icon = styled.img`
   width: 70%;
+  @media ${device.sm} {
+    padding-left: 25px;
+  }
 `;
--- src/Styles/StyledMusicSectionMiddle2.js
+++ src/Styles/StyledMusicSectionMiddle2.js
@@ -1,7 +1,11 @@
 import styled from "styled-components";
+import { device } from "../../breakpoints";
 
 export const StyledContainerHome = styled.div`
   margin: 0 37px;
+  @media ${device.sm} {
+    margin: 0 10px;
+  }
 `;
 // ------------------------
 export const Music = styled.div`
@@ -13,12 +17,21 @@ export const Music = styled.div`
 `;
 export const StyledDiv1 = styled.div`
   grid-area: 1 / 3 / 1 / 4;
+  @media ${device.sm} {
+  }
 `;
 export const StyledDiv2 = styled.div`
   grid-area: 1 / 1 / 1 / 2;
+  @media ${device.sm} {
+    width: 165%;
+  }
 `;
 export const StyledDiv3 = styled.div`
   grid-area: 2 / 2 / 3 / 3;
+  @media ${device.sm} {
+    grid-area: 2 / 2 / 3 / 3;
+    width: 148%;
+  }
 `;
 export const StyledDiv4 = styled.div`
   grid-area: 2 / 3 / 3 / 4;
@@ -27,29 +40,48 @@ export const StyledDiv4 = styled.div`
   text-shadow: rgb(0 0 0) 0 0 65px, rgb(255 255 255) 0 0 7px,
     rgb(255 255 255) 0 0 60px, #ffffff 0px 0px 70px, #ffffff 0px 0px 20px,
     #000000 0px 0px 20px, #ffffff 0px 0px 20px, #ffffff 0px 0px 20px;
+  @media ${device.sm} {
+    grid-area: 2 / 4 / 3 / 4;
+  }
 `;
 export const StyledDiv5 = styled.div`
   grid-area: 3 / 4 / 5 / 5;
+  @media ${device.sm} {
+    width: 154%;
+    grid-area: 3 / 3 / 5 / 4;
+  }
 `;
 export const StyledDiv6 = styled.div`
   grid-area: 3 / 5 / 5 / 6;
+  @media ${device.sm} {
+    grid-area: 3 / 5 / 5 / 6;
+  }
 `;
 export const StyledDiv7 = styled.div`
   grid-area: 3 / 2 / 5 / 3;
+  @media ${device.sm} {
+  }
 `;
 export const StyledDiv8 = styled.div`
   grid-area: 3 / 1 / 5 / 2;
+  @media ${device.sm} {
+    width: 110%;
+  }
 `;
 export const StyledDiv9 = styled.div`
   grid-area: 2 / 5 / 3 / 6;
+  @media ${device.sm} {
+  }
 `;
 export const StyledDiv10 = styled.div`
   grid-area: 2 / 5 / 4 / 7;
   text-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 20px #fff, 0 0 4px #550b3c,
     0 0 40px #550b3c;
   -webkit-text-stroke-color: hsla(0, 0%, 100%, 0.7215686274509804);
-
   font-size: 40px;
+  @media ${device.sm} {
+    font-size: 14px;
+  }
 `;
 
 export const Img1 = styled.img`
@@ -57,6 +89,8 @@ export const Img1 = styled.img`
   border-radius: 5px;
   box-shadow: 0 0 0.5rem #85d2ef, -5px -5px 10px 3px #4c006c,
     5px 5px 10px 4px #b9004e;
+  @media ${device.sm} {
+  }
 `;
 export const Img2 = styled.img`
   width: 100%;
@@ -64,6 +98,9 @@ export const Img2 = styled.img`
   filter: blur(3px);
   box-shadow: #ffffff 0px 0px 20px 2px, #ffffff 0px 0px 20px 4px,
     rgb(255 255 255) 0px 1px 11px 20px inset;
+  @media ${device.sm} {
+    filter: blur(0px);
+  }
 `;
 export const Img3 = styled.img`
   width: 100%;
@@ -71,6 +108,8 @@ export const Img3 = styled.img`
   box-shadow: rgba(240, 46, 170, 0.4) 5px 5px, rgba(240, 46, 170, 0.3) 10px 10px,
     rgba(240, 46, 170, 0.2) 15px 15px, rgba(240, 46, 170, 0.1) 20px 20px,
     rgba(240, 46, 170, 0.05) 25px 25px;
+  @media ${device.sm} {
+  }
 `;
 export const Img4 = styled.img`
   width: 100%;
@@ -81,10 +120,19 @@ export const Img4 = styled.img`
     rgb(255 255 255 / 88%) 0px 2px 1px, rgb(255 255 255 / 84%) 0px 4px 2px,
     rgb(255 255 255 / 72%) 0px 8px 4px, rgb(255 255 255 / 9%) 0px 16px 8px,
     rgb(255 255 255 / 9%) 0px 32px 16px;
+  @media ${device.sm} {
+  }
 `;
 export const Icon1 = styled.img`
   width: 120%;
+  @media ${device.sm} {
+    width: 165%;
+    padding-left: 15px;
+  }
 `;
 export const Icon = styled.img`
   width: 70%;
+  @media ${device.sm} {
+    width: 120%;
+  }
 `;
